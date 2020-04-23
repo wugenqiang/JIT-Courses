0e9fdcc5f8a8dd30090791d600a2561a821360b2
WuGenQiang <2422676183@qq.com>
2020-04-24 01:05:53 +0800

    课程攻略共享计划

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+echo '--------upload files start--------'
+
+# git init
+
+git add .
+
+git status
+
+git commit -m '课程攻略共享计划'
+
+echo '--------commit successfully--------'
+
+git push -u https://github.com/wugenqiang/JIT-Courses.git master
+
+echo '--------push to GitHub successfully--------'
